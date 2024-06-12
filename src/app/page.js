6ac6025943bf7065331a6ea79818051a876e55c2
rnargot <margot.murvihill@gmail.com>
2024-06-11 23:51:11 -0700

    third website of the day

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import "./page.css";
+
+export default function Home() {
+  return (
+    <div className="main">
+      <div className="rainy">RAINYHAUS</div>
+      <div className="container">
+        <div className="title"> i collect the essence of every space i occupy </div>
+      </div>
+      <div className="soon">they will be here soon</div>
+    </div>
+
+  );
+}
